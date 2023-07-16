3f68bbfea18123ca4fdb331a289efb91a245fe36
Easonlu0425 <andy1234532@hotmail.com>
2023-07-16 16:04:09 +0800

    feat:fix search function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,34 +1,45 @@
-const express = require('express')
-const router = express.Router()
-const Restaurants = require('../../models/restaurants')
+const express = require("express");
+const router = express.Router();
+const Restaurants = require("../../models/restaurants");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   const userId = req.user._id;
-  
-  Restaurants.find({userId})
+
+  Restaurants.find({ userId })
     .lean()
     .sort({ _id: "asc" })
     .then((restaurants) => {
       res.render("index", { restaurants });
     })
     .catch((error) => console.error(error));
-})
+});
 
-router.get('/search', (req, res) => {
+router.get("/search", (req, res) => {
+  const userId = req.user._id;
+  console.log(req.query.keywords)
   if (!req.query.keywords) {
-    return res.redirect('/')
+    return res.redirect("/");
   }
+  const keywords = req.query.keywords.trim().toLowerCase();
+  Restaurants.find({ userId })
+    .lean()
+    .sort({ _id: "asc" })
+    .then((data) => {
+      const restaurants = data.filter(
+        (restaurant) =>
+          restaurant.name.toLowerCase().includes(keywords) ||
+          restaurant.category.toLowerCase().includes(keywords)
+      );
+      console.log(restaurants)
+      return res.render("index", {
+        restaurants,
+        keywords,
+      });
+    });
+});
 
-  const keywords = req.query.keywords.trim().toLowerCase()
-  const filteredRestaurants = restaurants.results.filter(restaurant => 
-    restaurant.name.toLowerCase().includes(keywords) || 
-    restaurant.category.includes(keywords)
-  )
-  res.render('index', {restaurants : filteredRestaurants, keywords })
-})
+router.get("/create", (req, res) => {
+  res.render("new");
+});
 
-router.get('/create', (req, res) => {
-  res.render('new')
-})
-
-module.exports = router
+module.exports = router;
